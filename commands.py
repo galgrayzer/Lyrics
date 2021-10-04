9fcaa9dba8324f57dd3197db2d7c2aebe26c9af8
galgrayzer <67125898+galgrayzer@users.noreply.github.com>
2021-10-04 23:06:14 +0300

    updated for 10/4/21

--- commands.py
+++ commands.py
@@ -28,7 +28,7 @@ def play(song=None):
                 uris=['spotify:track:' + sp.search(song, type='track')['tracks']['items'][0]['id']])
         except:
             print('Error: Song not found')
-            sleep(0.5)
+            sleep(1)
 
 
 def skip():
@@ -41,15 +41,28 @@ def add(song):
                         ['tracks']['items'][0]['id'])
     except:
         print('Error: Song not found')
-        sleep(0.5)
+        sleep(1)
+
+
+def shuffle():
+    sp.shuffle(True)
+
+
+def prev():
+    sp.previous_track()
 
 
 def playlist(playlist):
+    try:
+        sp.start_playback(
+            context_uri='spotify:playlist:' + sp.search(playlist, type='playlist')['playlists']['items'][0]['id'])
+    except:
+        print('Error: Playlist not found')
+        sleep(1)
 
-    # spotify:playlist:18pn71gLfdDWPJ6z3Hbdiz
-    id = sp.search(playlist, type='playlist')['playlists']['items'][0]['id']
-    sp.start_playback()
-    sleep(100)
+
+def volume(num):
+    sp.volume(num)
 
 
 def main():
@@ -70,14 +83,46 @@ def main():
         elif user_input == 'skip':
             skip()
         elif 'add' in user_input:
-            user_input = user_input.replace('add ', '')
+            user_input = user_input.replace('add ', '', 1)
             add(user_input)
+        elif user_input == 'shuffle':
+            shuffle()
+        elif user_input == 'prev':
+            prev()
         elif 'playlist' in user_input:
-            user_input = user_input.replace('playlist ', '')
-            playlist(user_input)
+            if user_input == 'playlist':
+                print('Enter a playlist name')
+                sleep(1)
+            else:
+                user_input = user_input.replace('playlist ', '', 1)
+                playlist(user_input)
+        elif 'volume' in user_input:
+            if user_input == 'volume':
+                user_input = 100
+                volume(user_input)
+            else:
+                try:
+                    user_input = int(user_input.replace('volume ', '', 1))
+                    if user_input > 100 or user_input < 0:
+                        print('Volume must be betwin 0-100')
+                        sleep(1)
+                    else:
+                        volume(user_input)
+                except:
+                    print('Error: Invalid input')
+                    sleep(1)
+        elif user_input == 'volume_test':
+            i = 0
+            while i < 101:
+                volume(i)
+                i += 1
+                if i == 100:
+                    while i > -1:
+                        volume(i)
+                        i -= 1
         else:
             print('Invalid command')
-            sleep(0.5)
+            sleep(1)
 
 
 if __name__ == '__main__':
